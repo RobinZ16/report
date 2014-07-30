8aa37fb3b09420149554f0faed980913bc16b555
Zhiming Wang <zmwangx@gmail.com>
2014-07-29 20:54:45 -0700

    finish search with FLINT!
    Got a great speed boost!

--- misc/flint/search.cpp
+++ misc/flint/search.cpp
@@ -9,12 +9,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
+#include <time.h>
 
 #include "fmpqxx.h"
+#include "fmpzxx.h"
+#include "ulong_extras.h"
 
 using namespace flint;
 
-// constants
+// constant coefficients
 fmpqxx c1(1, 1u);
 fmpqxx c5(5, 1u);
 fmpqxx c6(6, 1u);
@@ -29,7 +33,8 @@ fmpqxx c80(80, 1u);
 fmpqxx c88(88, 1u);
 fmpqxx c132(132, 1u);
 
-fmpqxx rational_part(fmpqxx a, fmpqxx b, fmpqxx d) {
+inline fmpqxx rational_part(fmpqxx a, fmpqxx b, fmpqxx d)
+{
     return fmpqxx(pow(d,3) * pow(b,6) +                                 \
                                                                         \
                   pow(d,2) * (c22 * pow(b,4) + c40 * a * pow(b,4) +     \
@@ -44,7 +49,8 @@ fmpqxx rational_part(fmpqxx a, fmpqxx b, fmpqxx d) {
                    c22 * pow(a,4) + c8 * pow(a,5) + pow(a,6)));
 }
 
-fmpqxx irrational_part(fmpqxx a, fmpqxx b, fmpqxx d) {
+inline fmpqxx irrational_part(fmpqxx a, fmpqxx b, fmpqxx d)
+{
     return fmpqxx(pow(d,2) * (c8 * pow(b,5) + c6 * a * pow(b,5)) +      \
                                                                         \
                   d * (c22 * pow(b,3) + c88 * a * pow(b,3) +            \
@@ -56,6 +62,74 @@ fmpqxx irrational_part(fmpqxx a, fmpqxx b, fmpqxx d) {
                    c6 * pow(a,5) * b));
 }
 
+inline bool is_square(fmpzxx n)
+{
+    // if negative, not square
+    if (n < 0) {
+        return false;
+    }
+    fmpzxx sqrtn(sqrt(n));
+    return (sqrtn * sqrtn == n);
+}
+
+inline void test_x(fmpqxx d, slong p1, ulong q1, slong p2, ulong q2)
+{
+    // x = a + b sqrt(d), where a = p1/q1, b = p2/q2
+    fmpqxx a(p1, q1);
+    fmpqxx b(p2, q2);
+    // f(x) = fx1 + fx2 sqrt(d)
+    fmpqxx fx1(rational_part(a, b, d));
+    fmpqxx fx2(irrational_part(a, b, d));
+
+    // test if norm of f(x) is a rational square
+    fmpqxx norm(fx1 * fx1 - d * fx2 * fx2);
+    fmpzxx norm_num(norm.num());
+    fmpzxx norm_den(norm.den());
+    if (! is_square(norm_num) || ! is_square(norm_den)) {
+        return;
+    }
+
+    // print d, a, and b on one line, separated by a comma
+    std::cout << d << "," << a << ", " << b << std::endl;
+}
+
 int main(int argc, const char *argv[]) {
+    slong MAX_HEIGHT = atoi(argv[1]);
+    ulong UMAX_HEIGHT = MAX_HEIGHT;
+    slong MAX_D = atoi(argv[2]);
+
+    time_t start_time = time(NULL);
+
+    for (long d = 2; d <= MAX_D; ++d) {
+        // make sure squarefree
+        if (! n_is_squarefree(d)) {
+            continue;
+        }
+        fmpqxx d_rat(d, 1u); // convert d to fmpqxx
+        for (slong p1 = -MAX_HEIGHT; p1 <= MAX_HEIGHT; ++p1) {
+            for (ulong q1 = 1; q1 <= UMAX_HEIGHT; ++q1) {
+                // make sure p1 and q1 are coprime
+                if (n_gcd_full(abs(p1), q1) > 1) {
+                    continue;
+                }
+                for (slong p2 = -MAX_HEIGHT; p2 <= MAX_HEIGHT; ++p2) {
+                    // make sure p2 is nonzero, i.e., x is irrational
+                    if (p2 == 0) {
+                        continue;
+                    }
+                    for (ulong q2 = 1; q2 <= UMAX_HEIGHT; ++q2) {
+                        // make sure p2 and q2 are coprime
+                        if (n_gcd_full(abs(p2), q2) > 1) {
+                            continue;
+                        }
+                        test_x(d_rat, p1, q1, p2, q2);
+                    }
+                }
+            }
+        }
+        std::cerr << "finished d = " << d << "  ("                      \
+                  << difftime(time(NULL), start_time)                   \
+                  << " seconds elapsed)" << std::endl;
+    }
     return 0;
 }
